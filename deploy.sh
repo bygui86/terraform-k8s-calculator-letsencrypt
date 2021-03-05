2263f9b8199aca0580ea3767f92abfa9cd17db96
MPLeung <manbobo2012@gmail.com>
2021-03-05 07:28:31 +0000

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+git add .
+git commit -m "update"
+SHA=$(git rev-parse --short HEAD)
+
 docker build -t manbobo2002/multi-client:latest -t manbobo2002/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t manbobo2002/multi-server:latest -t manbobo2002/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t manbobo2002/multi-worker:latest -t manbobo2002/multi-worker:$SHA -f ./worker/Dockerfile ./worker
